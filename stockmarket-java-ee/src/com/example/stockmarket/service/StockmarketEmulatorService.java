ac27cb29e8bf172f62f0e76d7a0ccabe4e137473
deepcloudlabs <deepcloudlabs@gmail.com>
2021-02-25 19:34:55 +0300

    fix random price change strategy

--- stockmarket-java-ee/src/com/example/stockmarket/service/StockmarketEmulatorService.java
+++ stockmarket-java-ee/src/com/example/stockmarket/service/StockmarketEmulatorService.java
@@ -13,18 +13,17 @@ import com.example.stockmarket.entity.Stock;
 public class StockmarketEmulatorService {
 	@Inject
 	private StockMarketApplication stockMarketApp;
-	
-	@Schedule(second="*/5", hour="*", minute = "*", persistent = false)
+
+	@Schedule(second = "*/5", hour = "*", minute = "*", persistent = false)
 	public void updateStockPricesRandomly() {
-		stockMarketApp.findStocks(0, 10)
-		              .forEach( stock -> {
-		            	 var updatedStock = new Stock(stock); 
-		            	 updatedStock.setPrice( getRandomPrice(stock.getPrice()) );
-						 stockMarketApp.update(updatedStock);
-		              });  
+		stockMarketApp.findStocks(0, 10).forEach(stock -> {
+			var updatedStock = new Stock(stock);
+			updatedStock.setPrice(getRandomPrice(stock.getPrice()));
+			stockMarketApp.update(updatedStock);
+		});
 	}
 
 	private static double getRandomPrice(double oldPrice) {
-		return oldPrice * ( 1.0 + ThreadLocalRandom.current().nextDouble(-0.05,0.05) );
+		return oldPrice * (1.0 + 0.01 * (ThreadLocalRandom.current().nextDouble(1) - 0.5));
 	}
 }
